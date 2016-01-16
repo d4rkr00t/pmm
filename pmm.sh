1fe6765bd373fcc00ea0b8feb66dbf6a0dec9f4d
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-01-16 14:43:43 +0300

    feat(cli): Release info

--- pmm.sh
+++ pmm.sh
@@ -3,7 +3,17 @@ color_reset="\033[m"
 color_error="\033[1;31m"
 color_process="\033[0;34m"
 color_skiped="\033[1;33m"
-color_success="\033[1;32m"
+color_success="\033[0;32m"
+color_info="\033[0;35m"
+
+# Default tag
+TAG=${2:-latest}
+
+function __jsonval() {
+  KEY=$2
+  num=$3
+  cat $1 | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/\042'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
 
 function header() {
   echo
@@ -19,12 +29,19 @@ function header() {
 }
 
 function confirm() {
-  read -p $'\e[34mAre you sure you want to publish new version to npm?\e[0m (y/n) ' -n 1 -r
+  read -p $'\e[35mAre you sure you want to publish new version to npm?\e[0m (y/n) ' -n 1 -r
   echo && echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
     exit 1;
   fi
+}
+
+function releaseInfo() {
+  echo " ${color_info}Version:${color_reset}       " $(__jsonval package.json version)
+  echo " ${color_info}Latest commit:${color_reset} " $(git log -1 --oneline)
+  echo " ${color_info}Publish tag:${color_reset}   " $TAG
+  echo " ${color_info}Publisher:${color_reset}     " $(npm whoami --silent)
 }
 
 function runPrepare() {
@@ -60,9 +77,8 @@ function gitChecks() {
 header &&
 confirm &&
 
-echo "${color_process}Publishing" &&
-echo "---------------------------------------${color_reset}" &&
-echo &&
+echo "${color_process}Publishing${color_reset}" &&
+echo "${color_process}----------${color_reset}" &&
 
 echo "${color_process}[Running]${color_reset} Git checks." &&
 gitChecks &&
@@ -79,4 +95,8 @@ npm publish &&
 echo "${color_process}[Running]${color_reset} Pushing commits and tags to GitHub." &&
 git push --follow-tags &&
 
-echo "${color_success}Done!${color_reset}"
+echo &&
+echo "${color_success}[Done] ${color_success}Release info${color_reset}" &&
+echo "${color_success}--------------------${color_reset}" &&
+releaseInfo &&
+echo
